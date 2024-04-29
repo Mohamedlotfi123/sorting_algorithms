26fb81d53540632db0532f7abf0bbc5d6f9fb51f
root <root@DESKTOP-8VTPROF>
2024-04-30 02:28:03 +0300

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts array of integers using bubble sort algorithm
+ * @array: the array of integers.
+ * @size: number of element in the array.
+ *
+ * Return: Void.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array((const int *)array, size);
+			}
+		}
+	}
+}
