2ccb2374dd2f5a27bbb1eb74a953223dbb40c2a9
root <root@DESKTOP-8VTPROF>
2024-05-01 03:06:17 +0300

    Selection sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,7 @@
 #include "sort.h"
 /**
- * insertion_sort - function that sort a doubly linked list in ascending order.
+ * insertion_sort_list - function that sort a doubly linked list
+ *				in ascending order.
  * @list: pointer to pointer of the head of the list.
  *
  * Return: Void.
@@ -12,33 +13,32 @@ void insertion_sort_list(listint_t **list)
 	ptr = *list;
 	while (ptr != NULL)
 	{
-			if (tmp != NULL && ptr->n < tmp->n)
+		if (tmp != NULL && ptr->n < tmp->n)
+		{
+			while (tmp->prev != NULL && ptr->n < tmp->n)
+				tmp = tmp->prev;
+			if (tmp->prev != NULL)
 			{
-				while (tmp->prev != NULL && ptr->n < tmp->n)
-					tmp = tmp->prev;
-				if (tmp->prev != NULL)
-				{
-					(ptr->prev)->next = ptr->next;
-					(tmp->next)->prev = ptr;
-					ptr->next = tmp->next;
-					ptr->prev = tmp;
-					tmp->next = ptr;
-				}
-				else
-				{
-					tmp->next = ptr->next;
-					tmp->prev = ptr;
-					ptr->next = tmp;
-					ptr->prev = NULL;
-					if (tmp->next != NULL)
-						(tmp->next)->prev = tmp;
-					*list = ptr;
-				}
-				print_list(*list);
+				(ptr->prev)->next = ptr->next;
+				(tmp->next)->prev = ptr;
+				ptr->next = tmp->next;
+				ptr->prev = tmp;
+				tmp->next = ptr;
 			}
-			printf("%d\n", ptr->n);
-			tmp = ptr;
-			ptr = ptr->next;
-
+			else
+			{
+				tmp->next = ptr->next;
+				tmp->prev = ptr;
+				ptr->next = tmp;
+				ptr->prev = NULL;
+				if (tmp->next != NULL)
+					(tmp->next)->prev = tmp;
+				*list = ptr;
+			}
+			print_list(*list);
+		}
+		printf("%d\n", ptr->n);
+		tmp = ptr;
+		ptr = ptr->next;
 	}
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 /**
  * selection_sort - function that sorts an array of integers in ascending order
- * 			using the Selection sort algorithm
+ *			using the Selection sort algorithm
  * @array: array of integer
  * @size: size of the array
  *
