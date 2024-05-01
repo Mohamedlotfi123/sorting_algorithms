19180934a42526907fda3f54793bcfc31c8d444f
root <root@DESKTOP-8VTPROF>
2024-05-01 03:00:57 +0300

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * selection_sort - function that sorts an array of integers in ascending order
+ * 			using the Selection sort algorithm
+ * @array: array of integer
+ * @size: size of the array
+ *
+ * Return: Void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp = 0, index = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		tmp = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (tmp > array[j])
+			{
+				tmp = array[j];
+				index = j;
+			}
+		}
+		if (tmp != array[i])
+		{
+			array[index] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
+}
